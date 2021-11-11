f8a4f64ce473c0e06e8e63b44e59919c3b07f9e0
Charles Tacquet <ctacquet@outlook.fr>
2021-11-11 21:23:09 +0100

    Post 0.1

--- components/Posts.js
+++ components/Posts.js
@@ -5,21 +5,21 @@ const posts = [
         id: '123',
         username: 'redfive',
         userImg: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
-        img: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+        img: 'https://images.frandroid.com/wp-content/uploads/2020/12/streaming_guide.jpg',
         caption: "This is DOPE !"
     },
     {
         id: '123',
         username: 'redfive',
         userImg: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
-        img: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+        img: 'https://images.frandroid.com/wp-content/uploads/2020/12/streaming_guide.jpg',
         caption: "This is DOPE !"
     },
     {
         id: '123',
         username: 'redfive',
         userImg: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
-        img: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+        img: 'https://images.frandroid.com/wp-content/uploads/2020/12/streaming_guide.jpg',
         caption: "This is DOPE !"
     },
 ]
@@ -32,7 +32,8 @@ function Posts() {
                     key={post.id} 
                     id={post.id}
                     username={post.username}
-                    userImg={post.img}
+                    userImg={post.userImg}
+                    img={post.img}
                     caption={post.caption}
                 />
             ))}
