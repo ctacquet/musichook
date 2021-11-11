e0a80eed8c81db55177dccfb6176704e7002db76
Charles Tacquet <ctacquet@outlook.fr>
2021-11-11 23:19:24 +0100

    Post 0.2

--- components/Post.js
+++ components/Post.js
@@ -14,7 +14,7 @@ function Post({ id, username, userImg, img, caption }) {
         <div className="bg-white my-7 border rounded-sm">
             {/* Header */}
             <div className="flex items-center p-5">
-                <img src={userImg} className="rounded-full h-12 w-12 object-contain border p-1 mr-3" alt="" />
+                <img src={userImg} className="rounded-full h-12 w-12 object-cover border p-1 mr-3" alt="" />
                 <p className="flex-1 font-bold font-Poppins">{username}</p>
                 <DotsHorizontalIcon className="h-5" />
             </div>
