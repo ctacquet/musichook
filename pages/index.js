8ae23a7b99e254d8508c11a3bf1197eb177f4be7
Charles Tacquet <ctacquet@outlook.fr>
2021-11-16 20:18:02 +0100

    Meta infos

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ export default function Home() {
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>MusicHook</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Share your musics together !" />
+        <meta property="og:title" content="MusicHook" />
+        <meta property="og:description" content="Ce site me sert de portfolio, j'y poste principalement mes informations professionelles" />
+        <meta property="og:image" content="/preview.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
