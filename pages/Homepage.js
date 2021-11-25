c2a14759d80b3c84f0ffb005e8f2fac8d9bb20a9
Charles Tacquet <ctacquet@outlook.fr>
2021-11-25 23:06:39 +0100

    Update Homepage.js

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -1,21 +1,28 @@
-import Header from '../components/layouts/Header';
-import Feed from '../components/Feed';
+import Head from "next/head";
+import Header from "../components/layouts/Header";
 import LeftNavbar from "../components/layouts/LeftNavbar";
 import RightBar from "../components/layouts/RightBar";
-import Modal from '../components/Modal';
+import Footer from "../components/layouts/Footer";
+import Feed from "../components/Feed";
+import Modal from "../components/Modal";
 
 export default function Homepage() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
-      <Header pageTitle="Home"/>
-      
+      <Head>
+        <title>Home - MusicHook</title>
+      </Head>
+
+      <Header pageTitle="Home" />
+
       <main className="grid grid-cols-1 md:grid-cols-4 md:max-w-4xl xl:grid-cols-4 xl:max-w-8xl min-w-full px-8">
         <LeftNavbar />
         <Feed />
         <RightBar />
+        <Modal />
       </main>
-        
-      <Modal />
+
+      <Footer />
     </div>
-  )
+  );
 }
